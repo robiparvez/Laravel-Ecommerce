1791cd8c202364b3671061fc8ab70bc1b3ee92e7
Robi Parvez <parvezrobi@yahoo.com>
2017-12-19 18:49:00 +0600

    add admin column to users table

--- app/User.php
+++ app/User.php
@@ -2,28 +2,39 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'users';
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*
+     * Get the unique identifier for the user.
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->password;
+    }
+
+    /*
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getRemainderEmail()
+    {
+        return $this->email;
+    }
 }
