8345e5c7d878e67d770203eac19ba7a25806e182
Robi Parvez <parvezrobi@yahoo.com>
2017-12-19 13:38:17 +0600

    Starting Laravel E-commerce Basic Project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        Validator::extend('phone_number', function ($attribute, $value, $parameters)
+        {
+            return substr($value, 0, 2) == '01';
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
